7c4f07b611c9826b6471795603d5bfe4b79db042
IMMORTAL-coder <62939886+IMMORTAL-coder@users.noreply.github.com>
2020-03-31 19:39:22 +0530

    Demo for commit

--- app/src/main/java/com/example/bidkart/MainActivity.java
+++ app/src/main/java/com/example/bidkart/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //This an comment to see the push option on github
     }
 }
