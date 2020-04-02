31eaf814e2c539189650d9cd15650f158e262890
Sumer <sumerkhatri12@gmail.com>
2020-04-02 19:36:35 +0530

    Added Custom Toolbar

--- app/src/main/java/com/example/bidkart/Home.java
+++ app/src/main/java/com/example/bidkart/Home.java
@@ -1,14 +1,43 @@
 package com.example.bidkart;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Home extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        Toast.makeText(Home.this,id,Toast.LENGTH_LONG).show();
+        return true;
     }
 }
