63ea5ebe3d1be37ee5ff0e8e838de2113421c209
Sumer <sumerkhatri12@gmail.com>
2020-04-05 16:56:19 +0530

    Added Select Image From Gallery To Sell Now Activity

--- app/src/main/java/com/example/bidkart/SellProduct.java
+++ app/src/main/java/com/example/bidkart/SellProduct.java
@@ -1,14 +1,59 @@
 package com.example.bidkart;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 public class SellProduct extends AppCompatActivity {
+private ImageButton btn;
+private  static final int PICK_IMAGE=1;
+ImageView iv;
+private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_product);
+        btn=findViewById(R.id.imageButton);
+        iv=findViewById(R.id.imageView2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery=new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallery,"Select Image"),PICK_IMAGE);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+       super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case PICK_IMAGE:
+                    //data.getData returns the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+                    iv.setImageURI(selectedImage);
+                    break;
+            }
+
+
+
     }
 }
